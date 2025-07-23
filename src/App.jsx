deb7f7a90f260419768f5274e042036639b6c2b2
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-22 22:33:42 -0400

    TacoBuilder added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Counter from './Counter.jsx';
+import TacoBuilder from './TacoBuilder';
 
 function App() {
   return (
@@ -15,12 +16,15 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <Counter />
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="card">
+        <Counter />
+      </div>
+      <div>
+        <TacoBuilder />
+      </div>
     </>
   );
 }
